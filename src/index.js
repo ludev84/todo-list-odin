10dc69e693a4d494e568049798e51b672417e9fb
Luis <58019574+ludev84@users.noreply.github.com>
2026-04-10 04:49:16 -0600

    Template for projects and tasks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import Library from "./projects.js"
 
-const projects = new Library()
-projects.addProject("lala")
-projects.addTodo(projects.getProjects()[1].id, "titulo", "desc", "2026-03-29", 4)
-console.log(projects.getProjects())
+const library = new Library()
+library.addProject("proyecto1")
+library.addTodo(library.getProjects()[1].id, "titulo", "desc", "2026-03-29", 4)
+console.log(library.getProjects())
